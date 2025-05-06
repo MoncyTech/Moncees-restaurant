3a1b4996882a057fc3fc3c928b8b2287d8e159a1
Amjad-khan-fple <amjad@fpletech.in>
2025-05-06 11:25:47 +0530

    changed contactData

---
+++ src/constant/data.js
@@ -0,0 +1,27 @@
+// data.js
+export const contactData = [
+  {
+    icon: "Phone",
+    title: "Phone",
+    value: "+1 (555) 123-4567",
+  },
+  {
+    icon: "Mail",
+    title: "Email",
+    value: "info@monceesrestaurant.com",
+  },
+  {
+    icon: "MapPin",
+    title: "Location",
+    value: "123 Luxury Ave, Beverly Hills, CA 90210",
+  },
+  {
+    icon: "Clock",
+    title: "Hours",
+    value: [
+      "Mon-Thu: 11:30AM - 10:00PM",
+      "Fri-Sat: 11:30AM - 11:00PM",
+      "Sunday: 10:00AM - 9:00PM",
+    ],
+  },
+];
