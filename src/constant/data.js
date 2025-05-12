22b345662044ca004d998d54553b3ada5de21b9b
akhan.code <akhan.code@gmail.com>
2025-05-12 10:54:30 +0530

    open hours

--- src/constant/data.js
+++ src/constant/data.js
@@ -17,7 +17,7 @@ export const contactData = [
   },
   {
     icon: "Clock",
-    title: "Hours",
+    title: "Open hours",
     value: [
       "Mon-Thu: 1:00PM - 9:00PM",
       "Fri-Sat: 1:00PM - 11:00PM",
